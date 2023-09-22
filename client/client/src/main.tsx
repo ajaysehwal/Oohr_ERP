76dea3606ef8fe5ff6f4a0de2e85b3a1ef1f0ab9
ajaysehwal <ajaysehwal000@gmail.com>
2023-09-22 10:56:55 +0530

    updated

--- client/client/src/main.tsx
+++ client/client/src/main.tsx
@@ -4,8 +4,14 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 import './satoshi.css';
+import { CssBaseline } from '@mui/material';
+
+
 import AuthContextProvider from './context/AuthContext';
 import { ThemeProvider } from "@material-tailwind/react";
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+import "primereact/resources/primereact.min.css";                                       
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
@@ -13,7 +19,12 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Router>
       <AuthContextProvider>
       <ThemeProvider>
+      <CssBaseline />
+      <PrimeReactProvider>
       <App />
+      </PrimeReactProvider>
+     
+      
       </ThemeProvider>
       </AuthContextProvider>
      
